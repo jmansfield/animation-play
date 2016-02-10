7316240d4623364b96da0594eee9cf34bd10dec4
James Mansfield <james.mansfield@gmail.com>
2016-02-10 13:42:28 +1100

    Transition visibility demo

--- 04 Transforming rotate/application.js
+++ 04 Transforming rotate/application.js
@@ -1,12 +1,11 @@
 // This event handler waits for the page to load before executing the code inside to be run
 $(document).ready(function() {
-  var price = $('<p>From $399.99</p>'); // This shows how to put HTML into a variable
-  $('.vacation').append(price); //This add the content of the variable as the last child element of vacation
-
-  // Other ways of manipulating the DOM
-  $('.vacation').before("Before");
-  $('.vacation').after("After");
-  $('.vacation').prepend("Prepend");
-
-  $('button').remove(); //This removes the button element
+  $('.event-action').on('click', '.buy-button', function() {
+    $('.modal-overlay').addClass('active');
+    $('.modal').addClass('active');
+  });
+  $('.modal-header').on('click', '.modal-close', function() {
+    $('.modal-overlay').removeClass('active');
+    $('.modal').removeClass('active');
+  });
 });
